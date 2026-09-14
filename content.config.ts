33e98437eca8631f9e287ea385e71eb4dd690e5a
SIMULATAN <jakob@simulatan.me>
2026-09-14 21:15:45 +0200

    feat: Node Overview ft. Node Grid

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,26 @@
-import { defineCollection, defineContentConfig } from '@nuxt/content'
+import { defineCollection, defineContentConfig, z } from '@nuxt/content'
 
 export default defineContentConfig({
     collections: {
         glossary: defineCollection({
             type: 'page',
             source: 'glossary/**',
-        })
+        }),
+        nodes: defineCollection({
+            type: 'data',
+            source: 'nodes/**.json',
+            schema: z.object({
+                descriptor: z.object({
+                    name: z.string(),
+                }),
+                metadata: z.object({
+                    displayName: z.string(),
+                    icon: z.object({
+                        name: z.string(),
+                        color: z.string().nullable(),
+                    }).nullable(),
+                })
+            })
+        }),
     }
 })
---
+++ scripts/fetch-node-metadata.sh
@@ -0,0 +1,6 @@
+nodes_directory="$(git rev-parse --show-toplevel)/content/nodes"
+metadata="$(curl https://api.snoty.me/wiring/node/metadata -s)"
+
+while IFS= read -r line; do
+    echo "$line" | jq > "$nodes_directory/$(echo "$line" | jq -r '.descriptor.name').json"
+done < <(echo "$metadata" | jq -c '.[]')
