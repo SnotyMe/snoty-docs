903b0579c08bc61db3c83ddf935c6fea711f646d
SIMULATAN <jakob@simulatan.me>
2026-09-14 19:58:55 +0200

    feat: icon for external links

---
+++ app/components/content/ProseA.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps<{
+  href: string
+  target?: string
+  rel?: string
+}>()
+
+const isExternal = computed(() => props.href.startsWith('http') || props.href.startsWith('//'))
+</script>
+
+<template>
+  <ULink
+      :to="href"
+      :target="target"
+      :rel="rel"
+      :active="true"
+  >
+    <slot />
+    <UIcon
+        v-if="isExternal"
+        name="i-lucide-external-link"
+        class="inline-block ml-1 size-3.5 align-text-center opacity-70"
+    />
+  </ULink>
+</template>
